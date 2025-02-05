c3072f64b9565abcfebd3d0a7be0854806e18408
shehraan <shehraanhafiz@gmail.com>
2025-02-05 15:56:14 -0500

    CHANGED THE HOME PAGE

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SHEHRAAN HAFIZ</strong>
+                <strong className="main-name"> SHEHRAAN A HAFIZ</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
